57e05c0555b95b9a30b284da761031152c088128
Harrison <harrisonwolf42@gmail.com>
2026-02-23 11:34:31 -0800

    Fixed issue where polygon.h file was included in rand gen instead of polygon_new. Beginning dev of rand gen function.

--- random_polygon_generator.h
+++ random_polygon_generator.h
@@ -2,7 +2,7 @@
 #define RANDOM_POLYGON_GENERATOR_H
 //header file for function which generates a random polygon given an (optional) number of vertices and (optional) upper/lower coord bounds
 //need to decide whether to make the generator a class or just have a header file with a bunch of functions in it, like the STL headers
-#include "polygon.h"
+#include "polygon_new.h"
 
 /* Generate a random polygon with between 4 and 99 vertices, with coordinates from -250 to 250 */
 Polygon generate_random_polygon();
