5378c8de7d4a67d6a84426d0ac7e44d52e0c9d4a
Harrison <harrisonwolf42@gmail.com>
2026-02-23 14:56:21 -0800

    Began development of delaunay triang file, pointset file. Still need to decide on whether to internally store pointsets as unordered map or unordered set.

---
+++ delaunay_triangulation.h
@@ -0,0 +1,6 @@
+//header file for delaunay triangulation algorithm(s)
+#include <vector>
+#include "triangle.h"
+#include "pointset.h"
+
+std::vector<Triangle> bowyer_watson_triangulate(pointset
--- pointset.h
+++ pointset.h
@@ -4,6 +4,34 @@
 #define POINTSET_H
 
 #include "point.h"
+#include <unordered_set>
 #include <unordered_map>
+
+class Pointset{
+	private:
+		std::unordered_map<Point> points;
+		Polygon convex_hull;
+
+	public:
+		/* CONSTRUCTORS */
+
+		/*
+		 * Default constructor that makes an empty pointset
+		 */
+
+		/* ACCESSORS */
+		Point contains(double xval){ return points.contains }
+			
+
+		/* MUTATORS */
+		void add(Point p); //add a point to the pointset
+		void erase(Point p); //removes a point from the pointset 
+
+		/* MISC */
+};
+
+
+
+
 
 #endif
