91f7582ff344e96151786a44ab13f34a8eb573d6
harrisonwolf <112649772+harrisonwolf@users.noreply.github.com>
2026-02-27 16:28:54 -0800

    Add runtime-configurable tagged debug logging

--- rand_poly_gen_driver.cc
+++ rand_poly_gen_driver.cc
@@ -1,6 +1,7 @@
 //driver program for development of random_polygon_generator
 
 #include <iostream>
+#include <string>
 #include "polygon_new.h"
 #include "random_polygon_generator.h"
 #include "helper.h"
@@ -9,14 +10,19 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
+	logger::apply_runtime_inputs(argc, argv);
+
 	//TODO: Add arg for running program with given n
 	//don't really need to use DBG since this entire program is specifically to drive development for rand poly gen
 	//dbg statements belong in random poly gen file so that anywhere that that function is used, I can turn on debug statements and see what's going one
-	int NUMBER_OF_VERTICES = -1;
-	if(argc < 2){
-		NUMBER_OF_VERTICES = 7;
-	}else{
-		NUMBER_OF_VERTICES = stoi(argv[1]);
+	int NUMBER_OF_VERTICES = 7;
+	for(int i = 1; i < argc; ++i){
+		std::string arg = argv[i];
+		if(arg.rfind("--", 0) == 0){
+			continue;
+		}
+		NUMBER_OF_VERTICES = stoi(arg);
+		break;
 	}
 	cout << "Beginning random_polygon_generator driver program.\n";	
 	cout << "Testing first 3 points insertion...\n";
