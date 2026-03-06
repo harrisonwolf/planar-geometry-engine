10b4265dad04c4fd829c991269a34b560fe02fdd
harrisonwolf <112649772+harrisonwolf@users.noreply.github.com>
2026-03-05 16:17:08 -0800

    Fix Desmos API key and triangulation list rendering

--- tools/desmos-bridge/build-info.js
+++ tools/desmos-bridge/build-info.js
@@ -1,9 +1,9 @@
 window.__BUILD_INFO__ = {
   commit: {
-    full: "3f78a07df6093e09a437a5851b20d0e2f236e8c9",
-    short: "3f78a07"
+    full: "33e96ff0aa19f56b5ab8bf03a2bd00bd29c15598",
+    short: "33e96ff"
   },
   branch: "work",
   dirty: true,
-  buildTimeUtc: "2026-03-06T00:01:57Z"
+  buildTimeUtc: "2026-03-06T00:12:42Z"
 };
