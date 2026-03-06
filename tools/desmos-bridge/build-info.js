43b7b3dd9659bb40e97a6ce157374e1098faa7a1
harrisonwolf <112649772+harrisonwolf@users.noreply.github.com>
2026-03-05 16:04:53 -0800

    Improve Desmos API loading with key/version fallbacks

--- tools/desmos-bridge/build-info.js
+++ tools/desmos-bridge/build-info.js
@@ -1,9 +1,9 @@
 window.__BUILD_INFO__ = {
   commit: {
-    full: "91a9ca20aefb95adbb31666c2f8f07d59a18b368",
-    short: "91a9ca2"
+    full: "3f78a07df6093e09a437a5851b20d0e2f236e8c9",
+    short: "3f78a07"
   },
   branch: "work",
   dirty: true,
-  buildTimeUtc: "2026-03-05T23:39:53Z"
+  buildTimeUtc: "2026-03-06T00:01:57Z"
 };
