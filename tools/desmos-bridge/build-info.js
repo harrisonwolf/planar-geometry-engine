b509db9b590f784dfbf08ca5daa890e93eec4cbd
Harrison <harrisonwolf42@gmail.com>
2026-03-05 16:27:21 -0800

    Stop tracking js file

--- tools/desmos-bridge/build-info.js
+++
@@ -1,9 +0,0 @@
-window.__BUILD_INFO__ = {
-  commit: {
-    full: "3a54e9bac6ba9b2a7e902bef4a7874e27b736350",
-    short: "3a54e9b"
-  },
-  branch: "main",
-  dirty: false,
-  buildTimeUtc: "2026-03-06T00:21:24Z"
-};
